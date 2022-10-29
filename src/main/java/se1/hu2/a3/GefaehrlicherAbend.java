763ba377398a3274de0ab3c8e96b0a6769c75d73
christine <hoepfner.christine@gmail.com>
2022-10-29 13:13:01 +0200

    first commit

--- src/main/java/se1/hu2/a3/GefaehrlicherAbend.java
+++ src/main/java/se1/hu2/a3/GefaehrlicherAbend.java
@@ -1,4 +1,32 @@
 package se1.hu2.a3;
 
 public class GefaehrlicherAbend {
+
+    public static void main(String[] args) {
+
+        Villa riesenVilla = new Villa();
+        System.out.println("Villa unbewacht");
+
+        riesenVilla.zombieGreiftAn();
+
+        new Wachhund(riesenVilla);
+
+        riesenVilla.zombieGreiftAn();
+
+        new Alarmanlage(riesenVilla);
+        riesenVilla.zombieGreiftAn();
+
+    }
+}
+
+class Villa {
+/*    private Zombie zombie;
+
+    public Villa() {
+        zombie = new Zombie();
+    }*/
+
+    public void zombieGreiftAn() {
+//        zombie.biss();
+    }
 }
