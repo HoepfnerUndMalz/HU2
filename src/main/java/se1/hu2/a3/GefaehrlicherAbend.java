8254d31ba37479d0216e1f69c1c06c06bfdbbcfd
christine <hoepfner.christine@gmail.com>
2022-11-01 10:16:17 +0100

    finished

--- src/main/java/se1/hu2/a3/GefaehrlicherAbend.java
+++ src/main/java/se1/hu2/a3/GefaehrlicherAbend.java
@@ -1,9 +1,11 @@
 package se1.hu2.a3;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class GefaehrlicherAbend {
-
     public static void main(String[] args) {
-
         Villa riesenVilla = new Villa();
         System.out.println("Villa unbewacht");
 
@@ -14,19 +16,76 @@ public class GefaehrlicherAbend {
         riesenVilla.zombieGreiftAn();
 
         new Alarmanlage(riesenVilla);
+        new Wachmann(riesenVilla);
+
         riesenVilla.zombieGreiftAn();
-
     }
 }
-
 class Villa {
-/*    private Zombie zombie;
 
-    public Villa() {
-        zombie = new Zombie();
-    }*/
+    private List<Observer> observers = new ArrayList<Observer>();
 
-    public void zombieGreiftAn() {
-//        zombie.biss();
+    public void attach(Observer observer){
+        observers.add(observer);
+    }
+
+    public void zombieGreiftAn(){
+        System.out.println("(Einbruchsversuch)");
+        for (Observer observer : observers) {
+            observer.update();
+        }
+        if (observers.size() > 0){
+        System.out.println("Einbrecher läuft weg. Glück gehabt!");
+        } else {
+            System.out.println("Einbrecher raeumt Tresor aus ... und ist über alle Berge.");
+        }
+        System.out.println("");
     }
 }
+
+abstract class Observer {
+    protected Villa subject;
+    public abstract void update();
+}
+
+class Wachhund extends Observer{
+
+    public Wachhund(Villa subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("GRRR, RRRR ----- BEISS");
+    }
+}
+
+class Alarmanlage extends Observer{
+
+    public Alarmanlage(Villa subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("ALARM! ALARM!");
+    }
+}
+
+class Wachmann extends Observer{
+
+    public Wachmann(Villa subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Halt oder ich schieße!");
+    }
+}
+
+
+
